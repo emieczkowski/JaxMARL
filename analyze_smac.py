2321bc47514de3663341e24f76fa2bbc6a868006
Elizabeth Mieczkowski <bethmiecz@gmail.com>
2025-05-05 18:48:45 -0400

    config

--- analyze_smac.py
+++ analyze_smac.py
@@ -1,12 +1,12 @@
+from pathlib import Path
+from typing import Literal
+
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
-import numpy as np
-from scipy.stats import f_oneway
 
 df = pd.read_csv("smac_expt2/combined.csv")
-# df2 = pd.read_csv("smac_expt1/wandb_runs_chunk2.csv")
-# df = pd.concat([df1, df2], ignore_index=True)
 
 df = df[df["jsd"].notna()]
 
@@ -14,14 +14,15 @@ df = df[df["jsd"].notna()]
 df["trial"] = df["name"].apply(lambda x: "_".join(x.split("_")[:3]))
 df[["num_agents", "num_enemies"]] = df["trial"].str.extract(r"(\d+)m_vs_(\d+)m").astype(int)
 df["S"] = df["num_agents"]
+df["jsd"] = df["jsd"] / np.log2(df["num_agents"])
 
 def max_winrate_jsd(group):
     max_win = group["win_rate"].max()
     return group[group["win_rate"] == max_win]["jsd"].mean()
 
-# Apply that to each trial
 jsd_at_max_winrate = df.groupby("trial").apply(max_winrate_jsd).reset_index(name="jsd")
-
+jsd_at_max_winrate[["num_agents", "num_enemies"]] = jsd_at_max_winrate["trial"].str.extract(r"(\d+)m_vs_(\d+)m").astype(int)
+# jsd_at_max_winrate = jsd_at_max_winrate[jsd_at_max_winrate["num_enemies"] == 2]
 jsd_at_max_winrate[["num_agents", "num_enemies"]] = jsd_at_max_winrate["trial"].str.extract(r"(\d+)m_vs_(\d+)m").astype(int)
 jsd_at_max_winrate["S"] = jsd_at_max_winrate["num_agents"]
 
@@ -67,12 +68,10 @@ grouped = df.groupby("trial").agg({
     "S": "first"
 }).reset_index()
 
-# Extract agent/enemy counts and parallelizability
-# grouped[["num_agents", "num_enemies"]] = grouped["trial"].str.extract(r"(\d+)m_vs_(\d+)m").astype(int)
 grouped["S"] = grouped["num_agents"]
 
 # grouped = grouped[grouped["num_enemies"] == 2]
-
+print(grouped)
 pearson_corr, _ = pearsonr(grouped["S"], grouped["jsd"])
 print(f"Pearson correlation (S vs. JSD): {pearson_corr:.4f}")
 
@@ -92,6 +91,8 @@ grouped_by_agents = (
     .reset_index()
 )
 
+# grouped = grouped[grouped["num_enemies"] == 2]
+
 plt.figure(figsize=(8, 5))
 plt.bar(
     grouped_by_agents["num_agents"],
@@ -108,13 +109,13 @@ plt.grid(axis='y', alpha=0.3)
 plt.tight_layout()
 plt.show()
 
-filtered_df = df[df["num_enemies"] == 2]
+mean_jsd_by_agents = (
+    df.groupby("num_agents")["jsd"]
+    .mean()
+    .reset_index()
+    .rename(columns={"jsd": "mean_jsd"})
+)
 
-grouped_jsds = [
-    group["jsd"].values
-    for _, group in filtered_df.groupby("num_agents")
-    if len(group) > 1 
-]
-
-f_stat, p_value = f_oneway(*grouped_jsds)
-print(f"ANOVA F-statistic: {f_stat:.4f}, p-value: {p_value:.4e}")
+# Compute Pearson correlation between N and mean JSD
+pearson_corr, _ = pearsonr(mean_jsd_by_agents["num_agents"], mean_jsd_by_agents["mean_jsd"])
+print(f"Pearson correlation (mean JSD vs. N): {pearson_corr:.4f}")
